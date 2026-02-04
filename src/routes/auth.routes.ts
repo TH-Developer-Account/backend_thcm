838f284c75e8b813f821e052fc64b80766da5518
TH-Developer-Account <powerbi.dev@tatahitachi.co.in>
2026-02-04 14:44:11 +0530

    send otp login added

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -18,13 +18,13 @@ const router = Router();
 
 router.post("/register", asyncHandler(registerUser));
 router.post("/login", asyncHandler(loginWithPassword));
-router.post("/auth/reset-password", resetDefaultPassword);
-router.post("/auth/forgot-password", forgotPassword);
-router.get("/auth/reset-token/:token", verifyResetToken);
-router.post("/auth/reset-password/:token", resetPasswordWithToken);
+router.post("/reset-password", resetDefaultPassword);
+router.post("/forgot-password", forgotPassword);
+router.get("/reset-token/:token", verifyResetToken);
+router.post("/reset-password/:token", resetPasswordWithToken);
 router.post("/send-otp", asyncHandler(sendOtp));
 router.post("/verify-otp", asyncHandler(verifyOtp));
-// router.post("/refresh", asyncHandler(refreshAccessToken));
+router.post("/refresh", asyncHandler(refreshAccessToken));
 router.post("/logout", requireAuth, asyncHandler(logout));
 
 export default router;
