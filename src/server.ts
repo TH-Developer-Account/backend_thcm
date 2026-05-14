68039ded2bb9e435831e030711f213ca796a257f
TH-Developer-Account <powerbi.dev@tatahitachi.co.in>
2026-05-14 16:33:30 +0530

    minor port fix

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import dotenv from 'dotenv';
-import app from './app';
-import logger from './config/logger';
+import dotenv from "dotenv";
+import app from "./app";
+import logger from "./config/logger";
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
