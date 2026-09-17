4a2837c57472245818eb68a10d4f336a64a570ce
TH-Developer-Account <powerbi.dev@tatahitachi.co.in>
2026-09-17 15:24:47 +0530

    minor fix

--- src/app.ts
+++ src/app.ts
@@ -34,10 +34,10 @@ import ApiError from "@shared/utils/apiError";
 import { startJobs } from "@shared/jobs/scheduler";
 
 const corsOptions = {
-	origin: process.env.FRONTEND_URL, // frontend URL
-	methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-	allowedHeaders: ["Content-Type", "Authorization"],
-	credentials: true, // allow cookies / auth headers
+  origin: process.env.FRONTEND_URL, // frontend URL
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // allow cookies / auth headers
 };
 
 const app = express();
@@ -61,6 +61,7 @@ app.use("/api/v1/crf", crfRoutes);
 app.use("/api/v1/comment", commentRoutes);
 app.use("/api/v1/operator", operatorRoutes);
 app.use("/api/v1/leads", leadRoutes);
+app.use("/api/v1/business-partner", bpRoutes);
 app.use("/api/v1/pincodes", pincodeRoutes);
 app.use("/api/v1/report", reportRoutes);
 app.use("/api/v1/import", importRoutes);
@@ -71,14 +72,13 @@ app.use("/api/v1/vendor-onboarding", vendorOnboardRoutes);
 app.use("/api/v1/medi-claim", mediClaimRoutes);
 app.use("/api/v1/pdf", pdfRoutes);
 app.use("/api/v1/guest", guestRoutes);
-app.use("/api/v1/bp", bpRoutes);
 
 // Scheduler
 startJobs();
 
 /* 404 */
 app.use((req, res, next) => {
-	next(new ApiError(404, `Route not found: ${req.originalUrl}`));
+  next(new ApiError(404, `Route not found: ${req.originalUrl}`));
 });
 
 /* Global error handler */
